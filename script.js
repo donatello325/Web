bef5b7152f574f7b9b9f53d091fd6881a90bba7c
donatello325 <134037499+donatello325@users.noreply.github.com>
2024-10-26 00:50:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ let collection = JSON.parse(localStorage.getItem('collection')) || [];
 
 // Grupos de imágenes (4 combinaciones de 2 imágenes)
 const imageGroups = [
-    { left: 'img1_left.jpg', right: 'img1_right.jpg' },
-    { left: 'img2_left.jpg', right: 'img2_right.jpg' },
-    { left: 'img3_left.jpg', right: 'img3_right.jpg' },
+    { left: 'https://mfiles.alphacoders.com/793/thumb-793109.jpg', right: 'https://mfiles.alphacoders.com/944/thumb-944452.jpg' },
+    { left: 'https://www.omegacenter.es/90179-large_default/una-enfermedad-llamada-amor-04.jpg', right: 'https://images.cdn3.buscalibre.com/fit-in/360x360/a6/c0/a6c08d3fb14032dd22483419ff0f5371.jpg' },
+    { left: 'https://mfiles.alphacoders.com/971/thumb-971175.jpg', right: 'https://i.imgur.com/m0AVGGm.png' },
     { left: 'img4_left.jpg', right: 'img4_right.jpg' }
 ];
 
